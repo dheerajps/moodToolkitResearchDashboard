eae695becdeee494e2338e92fc15c2f25f88ea86
dheeraj <sdheeraj1992@gmail.com>
2017-03-07 17:15:22 -0600

    more alcohol DB stuff and stats, gracias Luke

---
+++ web/alcoholStudy/alcoholStudyService.js
@@ -0,0 +1,33 @@
+/** Custom Service which does the get request for ALCOHOL STUDY Data **/
+(function(){
+   angular.module('researchApp').service('alcoholStudyAPI',['$http', function alcoholStudyAPI($http){
+
+
+      var data ={ ID : ''};
+
+      return{
+
+         getalcoholStudyData: getalcoholStudyData,
+         getUser: getUser,
+         setUser: setUser
+
+      };
+      function getalcoholStudyData(){
+
+
+          var requestURL = '../../app/studies/alcoholStudyResponse.json';
+          return $http.get(requestURL);
+
+      }
+
+      function setUser(user){
+        data.ID = user;
+        console.log("user is set to "+ data.ID);
+      }
+
+      function getUser(){
+        console.log("sending user as "+ data.ID);
+        return data.ID;
+      }
+   }]);
+}) ();
